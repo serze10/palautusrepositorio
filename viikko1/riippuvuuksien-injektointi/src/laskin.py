c5a90fc82d1c8c85f5c40da04af3584f6bf49c3e
Santeri Silvennoinen <santeri.silvennoinen@helsinki.fi>
2025-11-03 19:10:54 +0200

    laskin modified

--- viikko1/riippuvuuksien-injektointi/src/laskin.py
+++ viikko1/riippuvuuksien-injektointi/src/laskin.py
@@ -4,18 +4,23 @@ class Laskin:
 
     def suorita(self):
         while True:
-            luku1 = int(self._io.lue("Luku 1:"))
+            try:
+                luku1 = int(self._io.lue("Luku 1:"))
+            except IndexError:
+                return
 
             if luku1 == -9999:
-                return
+                continue  # Siirry seuraavaan laskuun
 
-            luku2 = int(self._io.lue("Luku 2:"))
+            try:
+                luku2 = int(self._io.lue("Luku 2:"))
+            except IndexError:
+                return
 
             if luku2 == -9999:
-                return
+                continue  # Siirry seuraavaan laskuun
 
             vastaus = self._laske_summa(luku1, luku2)
-
             self._io.kirjoita(f"Summa: {vastaus}")
 
     def _laske_summa(self, luku1, luku2):
