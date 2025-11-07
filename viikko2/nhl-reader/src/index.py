1095abbae0c83dc093205d083a087f2461466f07
Santeri Silvennoinen <santeri.silvennoinen@helsinki.fi>
2025-11-07 10:49:23 +0200

    Players for nationality/pointsorted. added Rich

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,24 @@
+import requests
 from player import Player
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
 
+    players = [Player(player_dict) for player_dict in response]
+    
+    fin_players = [p for p in players if p.nationality == "FIN"]
+    
+    def by_points(player: Player):
+        return player.points()
+    
+    sorted_players = sorted(fin_players, key=by_points, reverse=True)
+
+    
+    print("Players from FIN:\n")
+    for player in sorted_players:
+        print(player)
+        
+        
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,13 @@
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
-    
+    def __init__(self, data: dict):
+        self.name = data['name']
+        self.nationality = data['nationality']
+        self.team = data['team']
+        self.goals = data['goals']
+        self.assists = data['assists']
+
+    def points(self):
+        return self.goals + self.assists
+
     def __str__(self):
-        return self.name
+        return f"{self.name:20} team {self.team:15}  goals {self.goals} assists {self.assists}"
