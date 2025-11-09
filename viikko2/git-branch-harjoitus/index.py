76b006ec80ae9671736feb9bf78d8759c5853a7d
Santeri Silvennoinen <santeri.silvennoinen@helsinki.fi>
2025-11-09 12:41:55 +0200

    tulo branch/tulo.py and index mod

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -10,6 +10,7 @@ x = int(input("luku 1: "))
 y = int(input("luku 2: "))
 print(f"{x} + {y} = {summa(x, y)}") # muutos mainissa
 print(f"{x} - {y} = {erotus(x, y)}") # muutos mainissa
+print(f"{x} * {y} = {tulo(x, y)}")
 
 logger("lopetetaan")
 print("goodbye!")
